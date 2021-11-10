5a54365568a8c707c73b7f71ddef67bab3be8613
Dexie <35111165+Lamby777@users.noreply.github.com>
2021-11-10 17:10:25 +0000

    Refactor class code and hash function

--- index.js
+++ index.js
@@ -2,44 +2,11 @@
 "use strict";
 
 // "Borrowed" from NodeJS documentation
-const crypto = require("crypto");
-const secret = process.env['SECRET'];
-const algo = crypto.createHmac('sha256', secret)
+const writeup = require("./writeup.js");
 const genesis = new Block(0, "", 1636563534866, {
-	//
+	sender: "Dex",
+	recipient: "Tomato",
+	content: {type: "Diamond", quantity: 1},
 });
 
 const blockchain = [genesis];
-
-class Block {
-	constructor(num, previous, body, timestamp, hash) {
-		this.num = num,
-		this.previous = previous,
-		this.body = body,
-		this.timestamp = timestamp,
-		this.hash = makeHashOf(this);
-	}
-
-	static makeHashOf(block) {
-		if (block instanceof Block) {
-			return hash(num + previous + body + timestamp);
-		} else {
-			throw TypeError("Attempt to get hash of non-block");
-		}
-	}
-
-	static generateNextBlock(body) {
-		let prevBlock = getLatestBlock();
-		let previous = prevBlock.hash;
-		let num = previous.index + 1;
-		let timestamp = new Date().getTime();//.toLocaleString(
-			//'en-US', { timeZone: 'America/New_York' });
-		let block = new Block(num, previous, body, timestamp);
-		//block.hash = makeHashOf(block);
-		return block;
-	};
-}
-
-function hash(input) {
-	return algo.update(input).digest("hex");
-}
---
+++ writeup.js
@@ -0,0 +1,45 @@
+/**
+* Code writeup for use in index.js
+*/ "use strict";
+
+const crypto = require("crypto");
+const secret = process.env['SECRET'];
+const algo = crypto.createHmac('sha256', secret)
+
+class Block {
+	constructor(num, previous, body, timestamp, hash) {
+		this.num = num,
+		this.previous = previous,
+		this.body = body,
+		this.timestamp = timestamp,
+		this.hash = makeHashOf(this);
+	}
+
+	static makeHashOf(block) {
+		if (block instanceof Block) {
+			return hash(num + previous + body + timestamp);
+		} else {
+			throw TypeError("Attempt to get hash of non-block");
+		}
+	}
+
+	static generateNextBlock(body) {
+		let prevBlock = getLatestBlock();
+		let previous = prevBlock.hash;
+		let num = previous.index + 1;
+		let timestamp = new Date().getTime();//.toLocaleString(
+			//'en-US', { timeZone: 'America/New_York' });
+		let block = new Block(num, previous, body, timestamp);
+		//block.hash = makeHashOf(block);
+		return block;
+	};
+}
+
+function hash(input) {
+	return algo.update(input).digest("hex");
+}
+
+module.exports = {
+	Block: Block,
+	hash: hash,
+}
