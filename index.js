c3e2f4a5da96ed898d55bf48dc3d422a709f12ee
Dexie <35111165+Lamby777@users.noreply.github.com>
2021-11-10 16:13:21 +0000

    algo -> sha256

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-const b = require("bcrypt");
+// Yet another random coin
+
+// "Borrowed" from NodeJS documentation
+const crypto = require("crypto");
+const secret = process.env['SECRET'];
+const algo = crypto.createHmac('sha256', secret)
 
 class Block {
 	constructor(num, timestamp, body, hash, previous) {
