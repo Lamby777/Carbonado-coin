3c72cfc6c102bd0f6056d39c1bee65f560cb28e3
Dexie <35111165+Lamby777@users.noreply.github.com>
2021-11-10 16:52:22 +0000

    "Next Block" method

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
+// Yet another random crypto coin
 "use strict";
-// Yet another random coin
 
 // "Borrowed" from NodeJS documentation
 const crypto = require("crypto");
@@ -7,12 +7,12 @@ const secret = process.env['SECRET'];
 const algo = crypto.createHmac('sha256', secret)
 
 class Block {
-	constructor(num, timestamp, body, hash, previous) {
+	constructor(num, previous, body, timestamp, hash) {
 		this.num = num,
-		this.timestamp = timestamp,
+		this.previous = previous,
 		this.body = body,
-		this.hash = hash,
-		this.previous = previous;
+		this.timestamp = timestamp,
+		this.hash = hash;
 	}
 
 	static makeHashOf(block) {
@@ -22,6 +22,16 @@ class Block {
 			throw TypeError("Attempt to get hash of non-block");
 		}
 	}
+
+	static generateNextBlock(body) {
+		let prevBlock = getLatestBlock();
+		let previous = prevBlock.hash;
+		let num = previous.index + 1;
+		let timestamp = new Date().getTime() / 1000;
+		let hash = hash(num + previous + body + timestamp);
+		let block = new Block(num, previous, body, timestamp, hash);
+		return block;
+	};
 }
 
 function hash(input) {
