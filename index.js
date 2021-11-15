d98f7b69ce9c8eb3008a6e6f591e0dd851dfc9dd
Dexie <35111165+Lamby777@users.noreply.github.com>
2021-11-15 09:16:02 +0000

    Proof of Work algorithm works

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ let {} = app.get("/", (req, res) => {
 	res.json(blockchain);
 });
 
-if (isMiner) (function () {
+if (isMiner) {
 
 	// Blockchain receive algorithm
 	let {} = app.post("/newBlock", (req, res) => {
@@ -52,29 +52,15 @@ if (isMiner) (function () {
 		}
 	});
 
-	// Main mining algorithm
-	function runCarbon(block) {
-		let solved = false;
-		let res;
-		let nonce = Math.floor(Math.random() * 10000);
-		do {
-			res = Block.hash(block, nonce, "binary");
-			if (false) { // If someone else solved
-				res = null;
-				break;
-			}
-		} while (!solved)
-
-		if (res) { // If didn't exit early
-			// Send results to other nodes
-		}
-	}
-})();
+	//
+};
 
 
 let {} = app.listen(PORT, () => {
 	console.log("Carbonado listening on port " + PORT);
 });
+
+let {} = runCarbon(genesis);
 
 // Functions
 
@@ -87,4 +73,67 @@ function blockchainLengthDilemma(newChain) {
 		verifyBlockchain(newChain)) {			// New chain valid
 		blockchain = newChain;
 	}
+}
+
+// Main mining algorithm
+function runCarbon(block) {
+	let res,
+		solved = false,
+		nonce = Math.floor(Math.random() * 10000);
+	
+	// Update difficulty
+	let difficulty = 18;
+
+	// Start hashing
+	do {
+		res = Block.hash(block, nonce, "hex");
+		if (hexToBinary(res)
+			.startsWith("0".repeat(difficulty))) solved = true;
+
+		// If someone else solved
+		if (false) {
+			res = null;
+			break;
+		}
+
+		nonce++;
+	} while (!solved)
+
+	if (res) { // If didn't exit early
+		console.log(hexToBinary(res));
+		console.log(nonce);
+		// Send results to other nodes
+		
+		// Then resolve promise
+		return res;
+	}
+}
+
+// Gonna be real with you, I took this straight from "The Stack"
+function hexToBinary(hex){
+	//hex = hex.replace("0x", "").toLowerCase();
+	var out = "";
+	for(var c of hex) {
+		switch(c) {
+			case '0': out += "0000"; break;
+			case '1': out += "0001"; break;
+			case '2': out += "0010"; break;
+			case '3': out += "0011"; break;
+			case '4': out += "0100"; break;
+			case '5': out += "0101"; break;
+			case '6': out += "0110"; break;
+			case '7': out += "0111"; break;
+			case '8': out += "1000"; break;
+			case '9': out += "1001"; break;
+			case 'a': out += "1010"; break;
+			case 'b': out += "1011"; break;
+			case 'c': out += "1100"; break;
+			case 'd': out += "1101"; break;
+			case 'e': out += "1110"; break;
+			case 'f': out += "1111"; break;
+			default: return "";
+		}
+	}
+
+return out;
 }
