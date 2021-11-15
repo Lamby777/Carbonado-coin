6ca4013a079b7eec30c55b61add55310a248aef1
Dexie <35111165+Lamby777@users.noreply.github.com>
2021-11-15 05:32:51 +0000

    Mining algorithm start

--- index.js
+++ index.js
@@ -1,35 +1,44 @@
 // Yet another random crypto coin
 "use strict";
 
-// "Borrowed" from NodeJS documentation
+// User Config Constants
+const isMiner = true; // True = Miner, False = Blockchain host
+
+// Code Constants
 const blockchain = [];
 const port = 11870;
 const {Block, hash} = require("./writeup.js")(blockchain);
 const Express = require("express");
 const app = Express();
+app.use(Express.json());
 
-const genesis = new Block(0, "", {
+const genesis = Block.generate({
 	sender: "Dex",
 	recipient: "Tomato",
 	content: {type: "Diamond", quantity: 1},
 });
-
-let a = Block.generate();
+Block.generate();
 
 console.log(blockchain);
-console.log(verifyBlockchain(blockchain));
 
-// Blockchain receive algorithm
-let {} = app.post("/nado/newBlock", (req, res) => {
-	if (true /* change to flag later*/) {
-		// Add new block to blockchain
-		Block.generate();
+if (isMiner) {
+	// Blockchain give algorithm (Reply to peers, like a simple torrent)
+	let {} = app.get("/", (req, res) => {
+		res.json(blockchain);
+	});
+
+	// Blockchain receive algorithm
+	let {} = app.post("/newBlock", (req, res) => {
+		if (true /* change to flag later*/) {
+			// Add new block to blockchain
+			Block.generate();
+		}
+	});
+
+	// Main mining algorithm
+	function runCarbon() {
+		//
 	}
-});
-
-// Main mining algorithm
-function runCarbon() {
-	//
 }
 
 
